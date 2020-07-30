ee8834ba8e0822f812e6cefff3cefffa9f7c449b
usherxd <usherxd@gmail.com>
2020-07-30 09:53:14 +0800

    add reviwes avg

--- AMreviews.py
+++ AMreviews.py
@@ -7,3 +7,8 @@ with open('reviews.txt','r') as f:
         if count % 1000 == 0:
             print(len(data))
 print('檔案讀取完畢，共', len(data), '筆')
+
+sun_len = 0
+for d in data:
+    sun_len += len(d)
+print('平均留言長度為', sun_len / len(data))
