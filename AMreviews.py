7bc2c32279098121d1a9b8ab05183f8440dbc6d5
usherxd <usherxd@gmail.com>
2020-08-08 21:34:00 +0800

    add word count and gressbar

--- AMreviews.py
+++ AMreviews.py
@@ -1,12 +1,35 @@
+import time
+import progressbar
+
 data = []
 count = 0
+bar = progressbar.ProgressBar(max_value = 1000000)
 with open('reviews.txt','r') as f:
     for line in f:
         data.append(line)
         count += 1
-        if count % 1000 == 0:
-            print(len(data))
+        bar.update(count)
 print('檔案讀取完畢，共', len(data), '筆')
+
+wc = {} # word_conut
+for d in data:
+    words = d.split(' ')
+    for word in words:
+        if word in wc:
+            wc[word] += 1
+        else:
+            wc[word] = 1  # 新增計數單字 
+
+while True:
+    word = input('請問你想查什麼字： ')
+    if word == 'q':
+        break
+    elif word in wc:   
+        print(word, '出現的次數為', wc[word])
+    else:
+        print('這個字沒有出現過哦！')
+
+print('~~~~~~~~~~感謝使用查詢')
 
 sun_len = 0
 for d in data:
